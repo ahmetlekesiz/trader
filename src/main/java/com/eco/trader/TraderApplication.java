9297ba0c41a216cf6d277516e998109e4a1c1975
Ahmet Lekesiz <ahmetlekesiz@gmail.com>
2020-11-04 16:31:57 +0300

    Implement exchange endpoint

--- src/main/java/com/eco/trader/TraderApplication.java
+++ src/main/java/com/eco/trader/TraderApplication.java
@@ -5,6 +5,8 @@ import com.eco.trader.util.scraper.runners.DefaultURLTesters;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,10 +24,14 @@ public class TraderApplication {
         return "Hello Trader";
     }
 
-    @RequestMapping("/usd")
-    public String getUSD() {
+    // localhost:8080/try?exchange=usd
+    @RequestMapping(value = "try", method = RequestMethod.GET)
+    public String getExchangeTRY(@RequestParam("exchange") String exchange) {
+        String url = "";
+        if (exchange.equals("usd")) url = "https://finance.yahoo.com/quote/TRY=X";
+        else if (exchange.equals("euro")) url = "https://finance.yahoo.com/quote/EURTRY=X";
         long start = System.currentTimeMillis();
-        yahooFinanceScraper = new YahooFinanceScraper("https://finance.yahoo.com/quote/TRY%3DX/community?p=TRY%3DX");
+        yahooFinanceScraper = new YahooFinanceScraper(url);
         String returnValue = String.valueOf(yahooFinanceScraper.getFinanceRunner().getCurrentValue());
         yahooFinanceScraper.getFinanceRunner().update();
         returnValue = String.valueOf(yahooFinanceScraper.getFinanceRunner().getCurrentValue()) + " " + String.valueOf(yahooFinanceScraper.getFinanceRunner().getChangeAmountAndRatio());
