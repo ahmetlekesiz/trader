0785afec0a4f2669177e0d215fba373851bf3f4a
Ahmet Lekesiz <ahmetlekesiz@gmail.com>
2020-11-04 13:08:38 +0300

    Add getUSD

--- src/main/java/com/eco/trader/TraderApplication.java
+++ src/main/java/com/eco/trader/TraderApplication.java
@@ -1,13 +1,38 @@
 package com.eco.trader;
 
+import com.eco.trader.util.scraper.YahooFinanceScraper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class TraderApplication {
+
+    private YahooFinanceScraper yahooFinanceScraper;
 
     public static void main(String[] args) {
         SpringApplication.run(TraderApplication.class, args);
     }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Trader";
+    }
+
+    @RequestMapping("/usd")
+    public String getUSD() {
+        long start = System.currentTimeMillis();
+        yahooFinanceScraper = new YahooFinanceScraper("https://finance.yahoo.com/quote/TRY%3DX/community?p=TRY%3DX");
+        String returnValue = String.valueOf(yahooFinanceScraper.getCurrentValue());
+        // finding the time after the operation is executed
+        long end = System.currentTimeMillis();
+        //finding the time difference and converting it into seconds
+        float sec = (end - start) / 1000F;
+        System.out.println(sec + " seconds");
+        return returnValue;
+    }
+
 
 }
